7122a7184623ca15d0a6bf533e84f303af2518b1
omokagbo <emmanuel.omokagbo@gmail.com>
2021-03-17 12:47:46 +0100

    working on variables, arrays and dictionaries

---
+++ app.swift
@@ -0,0 +1,11 @@
+var evenNumbers = [2, 4, 6, 8]
+
+var steps = ["shake it off", "eat it raw", "cook the rest"]
+
+
+var songs: [Any] = ["hello", "holla", "hella", "hiya", 3]
+
+var step = [Any]()
+var teps: [String] = []
+
+var both: [Any] = steps + songs
